a5a39be9dc478aecbb80dc58f7649b935023a00a
Naitik Tagline <naitikb.tagline@gmail.com>
2023-09-06 14:26:06 +0530

    Delete Exam

--- src/components/Teacher/Teacher.js
+++ src/components/Teacher/Teacher.js
@@ -4,28 +4,26 @@ import Loader from "../../reusable/Loader";
 import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "../../reusable/Button";
-import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 const Teacher = () => {
-  const navigate = useNavigate();
   const [viewExam, setViewExam] = useState([]);
   const [loading, setLoading] = useState(false);
+  const fetchExamData = async () => {
+    try {
+      const response = await apiAction({
+        method: "get",
+        url: "dashboard/Teachers/viewExam",
+        token: JSON.parse(localStorage.getItem("user-info"))?.token,
+        setLoading,
+      });
+
+      setViewExam(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await apiAction({
-          method: "get",
-          url: "dashboard/Teachers/viewExam",
-          token: JSON.parse(localStorage.getItem("user-info"))?.token,
-          setLoading,
-        });
-
-        setViewExam(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
+    fetchExamData();
   }, []);
 
   const handleOnClick = async (id) => {
@@ -41,9 +39,27 @@ const Teacher = () => {
       console.error("Error fetching data:", error);
     }
   };
+  const handleDeleteExam = async (id) => {
+    toast.success("Please wait processing your request");
+    try {
+      const response = await apiAction({
+        method: "delete",
+        url: "dashboard/Teachers/deleteExam",
+        token: JSON.parse(localStorage.getItem("user-info"))?.token,
+        id,
+        setLoading,
+      });
+      if (response.statusCode === 200) {
+        fetchExamData();
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   if (loading) {
     return <Loader />;
   }
+  console.log(viewExam);
   return (
     <div className="container ms-0 py-5">
       {viewExam.length === 0 ? (
@@ -53,7 +69,7 @@ const Teacher = () => {
           <div className="row d-flex justify-content-space-around">
             <div className="col custom-display">
               {viewExam.map((item, index) => (
-                <Card key={index}>
+                <Card key={index} className="m-2 w-50">
                   <Card.Body>
                     <Card.Title className="pb-2">
                       <i className="pe-2 mr-2 bi bi-book"></i>
@@ -79,12 +95,17 @@ const Teacher = () => {
                   <div className="text-center">
                     <Button
                       buttonText={"View Exam"}
-                      className={"btn btn-dark w-50 m-auto mb-2"}
+                      className={"btn btn-dark w-25 m-auto mb-2"}
                       onClick={() => handleOnClick(item._id)}
                     />
                     <Button
                       buttonText={"Edit Exam"}
-                      className={"btn btn-dark w-50 m-auto mb-2 ms-2"}
+                      className={"btn btn-dark w-25 m-auto mb-2 ms-2"}
+                    />
+                    <Button
+                      buttonText={"Delete Exam"}
+                      className={"btn btn-dark w-25 m-auto mb-2 ms-2"}
+                      onClick={() => handleDeleteExam(item._id)}
                     />
                   </div>
                 </Card>
@@ -93,6 +114,7 @@ const Teacher = () => {
           </div>
         </div>
       )}
+      <ToastContainer autoClose={2000} theme="colored" />
     </div>
   );
 };
